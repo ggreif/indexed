ce92a43deaafc1b9baff5387eb590641bd8a3d50
Gabor Greif <ggreif@gmail.com>
2012-09-27 14:59:32 +0200

    checkpoint

--- src/Indexed/Parser.hs
+++ src/Indexed/Parser.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE GADTs #-}
@@ -29,12 +32,44 @@ newtype Parser f a = Parser {
 
 type Failure f   r = Input f -> Added f -> More -> [String] -> String -> IResult f r
 type Success f a r = Input f -> Added f -> More -> a -> IResult f r
-data IResult f r = forall x. Fail (f x) [String] String
-                 -- | Partial (f ~> IResult f r)
-                 | forall x. Done (f x) r
+--data IResult f r = forall x. Fail (f x) [String] String
+--                 -- | Partial (f ~> IResult f r)
+--                 | forall x. Done (f x) r
+
+data R f r x = Fail' (f x) [String] String
+             | Partial (f x -> R f r x)
+             | Done' (f x) r
+type IResult f r = forall x. R f r x
+
 
 newtype Input f = I {unI :: forall x. f x}
 newtype Added f = A {unA :: forall x. f x}
 data More = Complete | Incomplete deriving (Eq, Show)
 
---instance IFunctor
+--instance IFunctor IResult
+--instance IFunctor Parser
+
+-- Build a thrist indexed by a pair of naturals
+
+data N = Z' | S' N
+
+data N' (n :: N) where
+  Z :: N' Z'
+  S :: N' n -> N' (S' n)
+
+data Interv :: (i, i) -> * where
+  P :: N' a -> N' b -> Interv '(a,b)
+
+{- I get:
+
+src/Indexed/Parser.hs:61:3:
+    Data constructor `P' cannot be GADT-like in its *kind* arguments
+      P :: forall (a :: N) (b :: N).
+           N' a -> N' b -> Interv N ('(,) N N a a)
+    In the definition of data constructor `P'
+    In the data declaration for `Interv'
+
+-}
+
+t1 :: Thrist Interv '(Z', Z')
+t1 = P Z Z :- P Z Z :- Nil
